1a58ef71c462012c027c0255cdf1ef2a4dea773b
Karina Masalova <masalova.karina@mail.ru>
2020-07-08 12:16:20 +0300

    feat: add modal window for item

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ const modalAdd = document.querySelector('.modal__add');
 const addAd = document.querySelector('.add__ad');
 const modalBtnSubmit = document.querySelector('.modal__btn-submit');
 const modalSubmit = document.querySelector('.modal__submit');
+const catalog = document.querySelector('.catalog');
+const modalItem = document.querySelector('.modal__item');
 
 addAd.addEventListener('click', () => {
   modalAdd.classList.remove('hide');
@@ -18,8 +20,17 @@ modalAdd.addEventListener('click', (event) => {
   }
 });
 
+catalog.addEventListener('click', (event) => {
+  const target = event.target;
+  modalItem.classList.remove('hide');
+  if(target.classList.contains('modal__close') || target === modalItem) {
+    modalItem.classList.add('hide');
+  }
+});
+
 document.addEventListener('keydown', (event) => {
   if(event.key === 'Escape') {
     modalAdd.classList.add('hide');
+    modalItem.classList.add('hide');
   }
 });
