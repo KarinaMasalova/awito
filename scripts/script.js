b72c85ace11b0295b0e776e4e2470ebcb6944fde
Karina Masalova <masalova.karina@mail.ru>
2020-07-11 11:31:29 +0300

    refactor: create closeModal function

--- scripts/script.js
+++ scripts/script.js
@@ -7,24 +7,28 @@ const modalSubmit = document.querySelector('.modal__submit');
 const catalog = document.querySelector('.catalog');
 const modalItem = document.querySelector('.modal__item');
 
+const closeModal = function(event) {
+  const target = event.target;
+  if(target.classList.contains('modal__close') || target === this) {
+    this.classList.add('hide');
+    if(this === modalAdd) {
+      modalSubmit.reset();
+    }
+  }
+}
+
 addAd.addEventListener('click', () => {
   modalAdd.classList.remove('hide');
   modalBtnSubmit.disabled = true;
 });
 
-modalAdd.addEventListener('click', (event) => {
-  const target = event.target;
-  if(target.classList.contains('modal__close') || target === modalAdd) {
-    modalAdd.classList.add('hide');
-    modalSubmit.reset();
-  }
-});
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
 
 catalog.addEventListener('click', (event) => {
   const target = event.target;
-  modalItem.classList.remove('hide');
-  if(target.classList.contains('modal__close') || target === modalItem) {
-    modalItem.classList.add('hide');
+  if(target.closest('.card')) {
+    modalItem.classList.remove('hide');
   }
 });
 
